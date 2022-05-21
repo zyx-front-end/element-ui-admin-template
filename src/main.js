e4c498a1db8f1c445bebf05ac334be28053e12ec
zyx-front-end <2195025326@qq.com>
2022-05-21 10:54:33 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,47 @@
+import Vue from 'vue'
+//引入公共的css文件
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+
+//引入element-ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import locale from 'element-ui/lib/locale/lang/en' // lang i18n
+//引入全局的scss文件
+import '@/styles/index.scss' // global css
+
+//引入app
+import App from './App'
+//引入vuex
+import store from './store'
+//引入路由
+import router from './router'
+//引入字体图标
+import '@/icons' // icon
+import '@/permission' // permission control
+
+/**
+ * If you don't want to use mock-server
+ * you want to use MockJs for mock api
+ * you can execute: mockXHR()
+ *
+ * Currently MockJs will be used in the production environment,
+ * please remove it before going online ! ! !
+ */
+// if (process.env.NODE_ENV === 'production') {
+//   const { mockXHR } = require('../mock')
+//   mockXHR()
+// }
+
+// set ElementUI lang to EN
+Vue.use(ElementUI, { locale })
+// 如果想要中文版 element-ui，按如下方式声明
+// Vue.use(ElementUI)
+
+Vue.config.productionTip = false
+
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+})
---
+++ src/settings.js
@@ -0,0 +1,21 @@
+module.exports = {
+  
+  //项目名称
+  title: 'Vue Admin Template',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+
+  //固定头部
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+
+  //左侧菜单logo
+  sidebarLogo: false
+}
